3267008766503a796946be4ad94414e72f4738ad
Lanou_铭 <1434640043@qq.com>
2018-11-19 17:41:35 +0800

    发送邮件Demo,定时任务Demo

--- email-demo/src/main/java/com/lanou3g/emaildemo/EmailDemoApplication.java
+++ email-demo/src/main/java/com/lanou3g/emaildemo/EmailDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+// 开启定时任务
 @EnableScheduling
 public class EmailDemoApplication {
 
--- email-demo/src/main/java/com/lanou3g/emaildemo/timing_task/ScheduledService.java
+++ email-demo/src/main/java/com/lanou3g/emaildemo/timing_task/ScheduledService.java
@@ -14,14 +14,19 @@ import org.springframework.stereotype.Component;
 @Async
 public class ScheduledService {
 
+    // 通过表达式来配置任务执行时间
     @Scheduled(cron = "0/5 * * * * *")
     public void scheduled(){
         log.info("=====>>>>>使用cron  {}",System.currentTimeMillis());
     }
+
+    // fixedRate：定义一个按一定频率执行的定时任务
     @Scheduled(fixedRate = 5000)
     public void scheduled1() {
         log.info("=====>>>>>使用fixedRate{}", System.currentTimeMillis());
     }
+
+    // 定义一个按一定频率执行的定时任务，与上面不同的是，改属性可以配合initialDelay， 定义该任务延迟执行时间。
     @Scheduled(fixedDelay = 5000)
     public void scheduled2() {
         log.info("=====>>>>>fixedDelay{}",System.currentTimeMillis());
