0f3f7212e026e6ab0699943082a41f12cd487b69
Lanou_铭 <1434640043@qq.com>
2018-12-11 17:02:31 +0800

    vue-demo-01

---
+++ vue_demo/src/main/java/com/lanou3g/vue_demo/VueDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lanou3g.vue_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueDemoApplication.class, args);
+    }
+}
